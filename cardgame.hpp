cb21f559dcc761095c463d8c66ff3a130816c838
Pancakem <pancakesdeath@protonmail.com>
2019-11-20 14:14:07 +0300

    Store player information in smart contract

--- cardgame.hpp
+++ cardgame.hpp
@@ -1,11 +1,28 @@
-#include <eosiolib/eosio.hpp>
+#include <eosio/eosio.hpp>
 
 using namespace std;
 using namespace eosio;
+
 class [[eosio::contract]] cardgame : public eosio::contract {
 
-  public:
+public:
+  cardgame(name receiver, name code, datastream<const char *> ds)
+      : contract(receiver, code, ds),
+        _users(receiver, receiver.value) {}
 
-    cardgame( name receiver, name code, datastream<const char*> ds ):contract(receiver, code, ds) {}
+  [[eosio::action]]
+  void login(name username);
 
+private:
+  struct [[eosio::table]] user_info {
+    name username;
+    uint16_t win_count = 0;
+    uint16_t lost_count = 0;
+
+    auto primary_key() const { return username.value; }
+  };
+
+  typedef eosio::multi_index<name("users"), user_info> users_table;
+
+  users_table _users;
 };
