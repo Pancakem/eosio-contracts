65685a11f49ddc1d2720ecc41142c4f774e367d9
Pancakem <pancakesdeath@protonmail.com>
2019-11-21 13:42:05 +0300

    Add startgame and playcard actions

--- cardgame.cpp
+++ cardgame.cpp
@@ -10,4 +10,42 @@ void cardgame::login(name username) {
   }
 }
 
-EOSIO_DISPATCH(cardgame, (login))
+void cardgame::login(name username) {
+  require_auth(username);
+
+  auto &user = _users.get(username.value, "User doesn't exist");
+
+  _users.modify(user, username, [&](auto& modified_user) {
+    game game_data;
+
+    for (uint8_t i = 0; i < 4; i++) {
+      draw_one_card(game_data.deck_player, game_data.hand_player);
+      draw_one_card(game_data.deck_ai, game_data.hand_ai);
+    }
+
+    modified_user.game_data = game_data;
+}
+
+void cardgame::playcard(name username, uint8_t player_card_idx) {
+    require_auth(username);
+
+    eosio_assert(player_card_idx < 4, "playcard: Invalid hand index");
+
+    auto &user = _users.get(username.value, "User doesn't exist");
+
+    eosio_assert(user.game_data.status == ONGOING,
+                 "playcard: This game has ended. Please start a new one");
+    eosio_assert(user.game_data.selected_card_player == 0,
+                 "playcard: The player has played his card this turn!");
+
+    _users.modify(user, username, [&](auto &modified_user) {
+      game &game_data = modified_user.game_data;
+
+      // Assign the selected card from the player's hand
+      game_data.selected_card_player = game_data.hand_player[player_card_idx];
+      game_data.hand_player[player_card_idx] = 0;
+    });
+
+}
+
+EOSIO_DISPATCH(cardgame, (login)(startgame)(playcard))
--- gameplay.cpp
+++ gameplay.cpp
@@ -1,1 +1,38 @@
 #include "cardgame.hpp"
+
+int cardgame::random(const int range) {
+  auto seed_iterator = _seed.begin();
+
+  if (seed_iterator == _seed.end()) {
+    seed_iterator = _seed.emplace( _self, [&]( auto& seed ) { });
+  }
+
+  int prime = 65537;
+  auto new_seed_value = (seed_iterator->value + now()) % prime;
+  
+  _seed.modify( seed_iterator, _self, [&]( auto& s ) {
+    s.value = new_seed_value;
+  });
+
+  int random_result = new_seed_value % range;
+  return random_result;
+}
+
+
+void cardgame::draw_one_card(vector<uint8_t>& deck, vector<uint8_t>& hand) {
+  int deck_card_idx = random(deck.size());
+
+  int first_empty_slot = -1;
+  for (int i = 0; i <= hand.size(); i++) {
+    auto id = hand[i];
+    if (card_dict.at(id).type == EMPTY) {
+      first_empty_slot = i;
+      break;
+    }
+  }
+  eosio_assert(first_empty_slot != -1, "No empty slot in the player's hand");
+
+  hand[first_empty_slot] = deck[deck_card_idx];
+  
+  deck.erase(deck.begin() + deck_card_idx);
+}
